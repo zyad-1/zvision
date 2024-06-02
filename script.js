732cc2146b1041eededff22960c2953755af52f1
zyad-1 <zyaddonga@gmail.com>
2024-06-02 16:14:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let navbar = document.querySelector('.header .navbar');
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
